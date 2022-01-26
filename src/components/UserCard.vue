731eda2908095b3575cdbbff276dd2430be2f9cf
Leo Yassuda <leo.yassuda@gmail.com>
2022-01-26 19:59:49 -0300

    feat: 🎸 update user card info using props values

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,20 +1,45 @@
 <template>
-    
     <div>
-        <span>{{msg}}</span>
+        <span>{{ msg }}</span>
     </div>
     <h2>ASDSADADA</h2>
+    <div>
+        <div
+            class="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-500 to-indigo-800"
+        >
+            <div
+                class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs"
+            >
+                <img
+                    class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto"
+                    :src="info?.avatar_url"
+                    alt="product designer"
+                />
+                <h1 class="text-lg text-gray-700">{{ info?.name }}</h1>
+                <h3 class="text-sm text-gray-400">{{ info?.login }}</h3>
+                <p class="text-xs text-gray-400 my-4">BIO: {{ info?.bio }}</p>
+                <a
+                    class="bg-indigo-600 px-8 py-2 mt-12 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide"
+                    :href="info?.html_url"
+                    target="_blank"
+                >Github Profile</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import User from "../model/User";
+import { defineComponent, PropType } from "vue";
+import { User } from "../model/User";
 
 export default defineComponent({
-  name: "owner-card",
-  props:{
-      msg: String,
-  },
-  methods: {},
+    props: {
+        msg: String,
+        info: {
+            type: Object as PropType<any>,
+            require: true
+        }
+    },
+    methods: {},
 });
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { computed, defineComponent, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { storeToRefs, mapActions } from 'pinia';
 import { userStore } from '../store/modules/user';
+import UserCard from '../components/UserCard.vue';
 
 const main = userStore();
 
@@ -15,30 +16,6 @@ onMounted(() => main.fetchInfo())
 
 <template>
   <h1>About Page</h1>
-  <owner-card msg="asd123"></owner-card>
-
-  <div>
-    <div
-      class="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-500 to-indigo-800"
-    >
-      <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
-        <img
-          class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto"
-          :src="info?.avatar_url"
-          alt="product designer"
-        />
-        <h1 class="text-lg text-gray-700">{{info?.name}}</h1>
-        <h3 class="text-sm text-gray-400">{{info?.login}}</h3>
-        <p
-          class="text-xs text-gray-400 my-4"
-        >BIO: {{info?.bio}}</p>
-        <a
-          class="bg-indigo-600 px-8 py-2 mt-12 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide"
-          :href="info?.html_url"
-          target="_blank"
-        >Github Profile</a>
-      </div>
-    </div>
-  </div>
+  <UserCard msg="asd123" :info="info"></UserCard>
 </template>
 
